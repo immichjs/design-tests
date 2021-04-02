b09cbf0147d93bb0bffd4a33313b75df4e555cda
Michel França <fr.mich17@gmail.com>
2021-04-02 17:23:53 -0300

    Estilização da interface do conversor de moedas concluído

--- currency-converter/cc-interface/src/App.vue
+++ currency-converter/cc-interface/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <div id="app">
-    <h1>Conversor de Reais (R$) para Dolar ($)</h1>
-    <input type="text" placeholder="R$" ref="input" />
-    <button @click="toConvert()">Calcular</button>
-    <p>{{ usd.name }}</p>
-    <p>R$ {{ afterBRL }} = $ {{ convertedDollar }}</p>
+  <div id="app" class="flex items-center justify-center h-screen bg-purple-100">
+    <form class="flex flex-col gap-4 p-12 bg-white rounded shadow-lg w-min">
+      <h1 class="text-2xl text-center text-gray-400">
+        Real Brasileiro para {{ usd.name }}
+      </h1>
+      <input type="text" placeholder="R$" ref="input" :class="inputStyle" />
+      <input
+        type="text"
+        placeholder="$"
+        v-model="convertedDollar"
+        :class="inputStyle"
+      />
+      <button
+        @click="toConvert($event)"
+        class="p-4 mx-10 text-lg font-medium text-white duration-200 transform bg-purple-600 rounded-full hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 hover:scale-105"
+      >
+        Calcular
+      </button>
+    </form>
   </div>
 </template>
 
@@ -16,9 +29,11 @@ export default {
   data() {
     return {
       usd: [],
+      beforeBRL: 0,
       afterBRL: 0,
-      brl: 0,
-      convertedDollar: 0,
+      convertedDollar: '',
+      inputStyle:
+        'text-center border-2 rounded-lg p-3 px-20 text-lg font-medium bg-transparent outline-none text-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent duration-200',
     };
   },
   created() {
@@ -29,24 +44,16 @@ export default {
       });
   },
   methods: {
-    toConvert() {
+    toConvert(event) {
+      event.preventDefault();
+      this.beforeBRL = this.$refs['input'].value;
       this.afterBRL = this.$refs['input'].value;
-      this.brl = this.$refs['input'].value;
 
-      const convertedValue = (this.brl *= this.usd.bid);
-      this.convertedDollar = convertedValue.toFixed(2);
-      this.$refs['input'].value = '';
+      const convertedValue = (this.afterBRL *= this.usd.bid);
+      this.convertedDollar = `$ ${convertedValue.toFixed(2)}`;
     },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/tailwind.css"></style>
