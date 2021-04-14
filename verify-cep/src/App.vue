9ef86e1f0c52d94b9cce3769489d190cecb468cb
Michel França <fr.mich17@gmail.com>
2021-04-14 06:02:34 -0300

    Recebendo dados do ViaCep API concluído

---
+++ verify-cep/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: {}
+    }
+  },
+  created() {
+    axios.get('https://viacep.com.br/ws/18072070/json').then(response => {
+      this.data = response.data
+      console.log(this.data)
+    })
+  }
+}
+</script>
+
+<style lang="postcss">
+</style>
